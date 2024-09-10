81be6576bd30451138dde90a6e1fd4fe9b2aad66
Juhee <hero3j@gmail.com>
2024-09-10 18:01:17 +0900

    프로토타입 스코프-싱글톤 빈과 함께 사용 JSR-330 Provider

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 public class SingletonWithPrototypeTest1 {
 
@@ -50,11 +51,14 @@ public class SingletonWithPrototypeTest1 {
         //    this.prototypeBean = prototypeBean;
         //}
 
+        //@Autowired
+        //private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+
         @Autowired
-        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+        private Provider<PrototypeBean> prototypeBeanProvider;
 
         public int logic() {
-            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
